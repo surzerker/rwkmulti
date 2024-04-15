8dbaaef7d17a0585a1d01f4c7aad1900bf4768a5
Surzerker (S2) <126700262+surzerker@users.noreply.github.com>
2024-04-15 12:52:28 -0400

    Update arrange.py

--- arrange.py
+++ arrange.py
@@ -14,10 +14,22 @@ def tile_windows(layout):
         width = screensize.width // 2
         height = screensize.height // 2
         positions = [(0, 0), (width, 0), (0, height), (width, height)]
+    elif layout == '2x3':
+        width = screensize.width // 2
+        height = screensize.height // 3
+        positions = [(0, 0), (width, 0), (0, height), (width, height), (0, 2 * height), (width, 2 * height)]
+    elif layout == '3x2':
+        width = screensize.width // 3
+        height = screensize.height // 2
+        positions = [(0, 0), (width, 0), (2 * width, 0), (0, height), (width, height), (2 * width, height)]
     elif layout == '4x1':
         width = screensize.width // 4
         height = screensize.height
         positions = [(i * width, 0) for i in range(4)]
+    elif layout == '6x1':
+        width = screensize.width // 6
+        height = screensize.height
+        positions = [(i * width, 0) for i in range(6)]
     else:
         print("Unsupported layout")
         return
@@ -27,5 +39,5 @@ def tile_windows(layout):
         win.resizeTo(width, height)
 
 # Example usage
-layout = '4x1'  # Choose '2x2' or '4x1'
+layout = '4x1'  # Choose '2x2', '2x3', '3x2', '4x1' or '6x1'
 tile_windows(layout)
